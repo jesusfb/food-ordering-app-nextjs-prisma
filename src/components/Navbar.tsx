ce982ec2703e33f01a0bb76cf7c3206039bb68f2
unknown <ramovic225@gmail.com>
2024-08-30 19:22:59 +0200

    style changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,16 +22,16 @@ const Navbar = () => {
                 <div className="relative hidden md:block w-[80%] mx-auto drop-shadow-2xl py-6 px-10 bg-white">
                     <div className="flex justify-between">
                         <h1 className="text-2xl font-bold">Logo</h1>
-                        <div className="flex space-x-12">
+                        <div className="flex space-x-12 items-center">
                             {navMenu.map((menu, index) => (
                                 <Link href={menu.href} key={index}>
                                     <p>{menu.name}</p>
                                 </Link>
                             ))}
-                            <CartIcon />
-                            <Link href={"/login"}>
+                            <Link href={"/login"} className="bg-gray-800 text-gray-100 text-xl px-4 py-2 uppercase hover:bg-gray-700 duration-300">
                                 <p>Login</p>
                             </Link>
+                            <CartIcon />
                         </div>
                     </div>
                 </div>
@@ -48,15 +48,17 @@ const Navbar = () => {
                         </div>
                 </div>
                 <div className={`absolute md:hidden  w-full bg-white shadow py-2 px-2 ${isOpened ? 'translate-y-0 opacity-100' : '-translate-y-[80vh] opacity-0'} duration-300`}>
-                    <div className="flex flex-col space-y-4 items-center justify-center z-10">
+                    <div className="flex flex-col items-center justify-center z-10">
                         {navMenu.map((menu, index) => (
                             <Link href={menu.href} key={index}  className="w-full text-center" onClick={() => setIsOpened(false)}>
                                 <p className="text-xl py-6 w-full hover:bg-gray-800 hover:text-white cursor-pointer duration-300">{menu.name}</p>
                             </Link>
                         ))}
-                        <CartIcon className={'text-xl py-6 w-full hover:bg-gray-800 hover:text-white cursor-pointer duration-300'} onClick={() => setIsOpened(false)}/>
+                        <div className={`w-full flex justify-center items-center hover:bg-gray-800 hover:text-white py-4 duration-300 cursor-pointer`} onClick={() => setIsOpened(false)}>
+                            <CartIcon />
+                        </div>
                         <Link href={"/login"} className="w-full text-center" onClick={() => setIsOpened(false)}>
-                            <p className="text-xl py-6 w-full hover:bg-gray-800 hover:text-white cursor-pointer duration-300">Login</p>
+                            <p className="text-xl uppercase font-semibold py-6 w-full hover:bg-gray-800 hover:text-white cursor-pointer duration-300">Login</p>
                         </Link>
                     </div>
                 </div>
