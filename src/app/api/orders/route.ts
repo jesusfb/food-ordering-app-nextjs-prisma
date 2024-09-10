19f38b9ed3197600f323f468dea40dc40177ec8c
unknown <ramovic225@gmail.com>
2024-09-10 22:07:35 +0200

    orders route created

---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,33 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/utils/connection";
+import { getAuthSession } from "@/utils/auth";
+
+
+export const GET = async (req:NextRequest) => {
+    
+    const session = await getAuthSession()
+    if(session) {
+        try {
+            if(session.user.isAdmin) {
+                const orders = prisma.order.findMany()
+                return new NextResponse(JSON.stringify(orders), { status: 200 })
+            }
+            const orders = prisma.order.findMany({
+                where: {
+                    userEmail:session.user.email!
+                }
+            })
+            return new NextResponse(JSON.stringify(orders), { status: 200})
+        } catch (err) {
+            return new NextResponse(
+                JSON.stringify({message: "Internal server error"}),
+                {status: 500}
+            );
+        }
+    } else {
+        return new NextResponse(
+        JSON.stringify({ message: "You are not authentificated!" }),
+         {status: 401}
+         );
+    }
+}
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,14 +3,45 @@ import { NextAuthOptions, getServerSession } from "next-auth";
 import Google from "next-auth/providers/google"
 import { prisma } from "./connection";
 
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      isAdmin: Boolean
+    }
+  }
+}
+declare module "next-auth/jwt" {
+  interface JWT {
+      isAdmin: Boolean
+  }
+}
+
 export const authOptions:NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt"
+  },
   providers: [
     Google({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     })
-  ]
+  ],
+  callbacks: {
+    async session({token, session}) {
+      if(token) {
+      session.user.isAdmin = token.isAdmin
+      }
+    },
+    async jwt({token}) {
+      const userInDd = await prisma.user.findUnique({
+        where:{
+          email: token.email!,
+        },
+      })
+      token.isAdmin = userInDd?.isAdmin!;
+    }
+  }
 }
 
 export const getAuthSession = () => getServerSession(authOptions)
