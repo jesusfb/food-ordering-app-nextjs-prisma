92d01ee607c04bd3010f81ff4e332ab114bc89e0
unknown <ramovic225@gmail.com>
2024-09-15 00:12:44 +0200

    small changes in add page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -13,14 +13,14 @@ const AddProduct = () => {
     }
 
     return (
-        <div className="h-screen flex justify-center items-center flex-col">
-            <div className="flex justify-center flex-col w-full md:w-[800px] px-10 md:shadow-xl shadow-black">
+        <div className="min-h-screen flex justify-center items-center flex-col">
+            <div className="flex justify-center flex-col w-full md:w-[800px] px-10 mt-10 md:shadow-xl shadow-black">
                 <h1 className="text-2xl font-semibold text-center py-4">Add new product</h1>
                 <form className="flex flex-col flex-wrap gap-2 w-full">
                     <div className="flex flex-col justify-center items-center gap-2">
-                        <div className="bg-gray-100 h-[200px] w-[150px]">
+                        <div className="bg-gray-100 h-[150px] w-[150px]">
                         </div>
-                        <div className="text-center bg-gray-800 text-gray-100 font-bold py-2 uppercase hover:bg-gray-700 duration-300 w-[150px] cursor-pointer" onClick={handleImageUpload}>
+                        <div className="text-center bg-gray-800 text-gray-100 font-semibold py-2 uppercase hover:bg-gray-700 duration-300 w-[150px] cursor-pointer" onClick={handleImageUpload}>
                             <input type="file" className="hidden" ref={imageRef} />
                             <p>Upload Image</p>
                         </div>
@@ -46,6 +46,10 @@ const AddProduct = () => {
                         <div className="flex gap-2">
                             <input placeholder="Type name..." className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none" />
                             <input placeholder="Type additional price..." className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none" />
+                        </div>
+                        <div className="flex justify-between mt-2 flex-wrap">
+                            <button className="text-center bg-gray-800 text-gray-100 px-4 py-2 font-semibold uppercase hover:bg-gray-700 duration-300">Add option</button>
+                            <p className="px-4 ring-1 ring-gray-800 flex items-center text-gray-800">Option 1</p>
                         </div>
                     </div>
                     <button className="w-full text-center bg-gray-800 text-gray-100 text-xl font-bold px-4 py-2 uppercase hover:bg-gray-700 duration-300 my-4">Add product</button>
