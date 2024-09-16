04acc9b2a7c6976ef38183c9140696a6eeb2d090
unknown <ramovic225@gmail.com>
2024-09-16 21:08:12 +0200

    option add functionality added

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,15 +1,48 @@
 "use client"
 
-import { useRef } from "react"
+import { useRef, useState } from "react"
+
+type Option = {
+    optionName: string,
+    additionalPrice: number
+}
+type Inputs = {
+    name: string,
+    description: string,
+    price: number,
+    categorySlug: string,
+}
 
 const AddProduct = () => {
 
     const imageRef = useRef<HTMLInputElement | null>(null);
+    const [inputs, setInputs] = useState<Inputs>({
+        name: "",
+        description: "",
+        price: 0,
+        categorySlug: ""
+    });
+    const [option, setOption] = useState<Option>({
+        optionName: "",
+        additionalPrice: 0,
+    });
+    const [options, setOptions] = useState<Option[]>([]);
 
     const handleImageUpload = () => {
         if (imageRef.current) {
             imageRef.current.click();
         }
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setInputs(prevState => {
+            return { ...prevState, [e.target.name]: e.target.value }
+        })
+    }
+    const handleChangeOption = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setOption(prevState => {
+            return { ...prevState, [e.target.name]: e.target.value }
+        })
     }
 
     return (
@@ -27,29 +60,62 @@ const AddProduct = () => {
                     </div>
                     <div className="flex flex-col gap-1 w-full">
                         <label htmlFor="">Name</label>
-                        <input placeholder="Type name..." className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none" />
+                        <input placeholder="Type name..."
+                            className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none"
+                            name="name"
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="flex flex-col gap-1 w-full">
                         <label htmlFor="">Description</label>
-                        <textarea placeholder="Type description..." className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none" />
+                        <textarea placeholder="Type description..."
+                            className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none"
+                            name="description"
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="flex flex-col gap-1 w-full">
                         <label htmlFor="">Price</label>
-                        <input placeholder="Type price..." className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none" />
+                        <input placeholder="Type price..."
+                            className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none"
+                            name="price"
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="flex flex-col gap-1 w-full">
                         <label htmlFor="">Category</label>
-                        <input placeholder="Type category..." className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none" />
+                        <input placeholder="Type category..."
+                            className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none"
+                            name="category"
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="flex flex-col gap-1 w-full">
                         <label htmlFor="">Options</label>
                         <div className="flex gap-2">
-                            <input placeholder="Type name..." className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none" />
-                            <input placeholder="Type additional price..." className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none" />
+                            <input placeholder="Type name..."
+                                className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none"
+                                name="optionName"
+                                onChange={handleChangeOption}
+                            />
+                            <input type="number" 
+                                placeholder="Type additional price..."
+                                className="shadow-sm shadow-black font-bold px-1 py-2 w-full focus:outline-none"
+                                name="additionalPrice"
+                                onChange={handleChangeOption}
+                            />
                         </div>
-                        <div className="flex justify-between mt-2 flex-wrap">
-                            <button className="text-center bg-gray-800 text-gray-100 px-4 py-2 font-semibold uppercase hover:bg-gray-700 duration-300">Add option</button>
-                            <p className="px-4 ring-1 ring-gray-800 flex items-center text-gray-800">Option 1</p>
+                        <div className="flex justify-between mt-2 flex-wrap gap-2">
+                            <div className="text-center bg-gray-800 text-gray-100 px-4 py-2 font-semibold uppercase hover:bg-gray-700 duration-300 cursor-pointer" onClick={() => setOptions(prevState => [...prevState, option])}>Add option</div>
+                            <div className="flex flex-wrap gap-2">
+                            {options.map(option => (
+                                <div className="px-4 ring-1 ring-gray-800 flex items-center text-gray-800 gap-1" key={option.optionName}>
+                                    <p>{option.optionName}</p>
+                                    <p>-</p>
+                                    <p>${option.additionalPrice}</p>
+                                </div>
+                            ))}
+                            </div>
                         </div>
                     </div>
                     <button className="w-full text-center bg-gray-800 text-gray-100 text-xl font-bold px-4 py-2 uppercase hover:bg-gray-700 duration-300 my-4">Add product</button>
