346c6bedb888384385742899733aef05a2990d29
unknown <ramovic225@gmail.com>
2024-09-17 20:23:21 +0200

    small changes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Footer = () => {
+
     return (
-        <div className="flex text-center justify-center gap-4 py-2 font-light text-gray-800">
+        <div className="flex text-center justify-center gap-2 py-2 font-light text-gray-800">
+           <p>{new Date().getFullYear()}</p>
            <p>© All rights reserved</p>
            <p>-</p>
            <p className="lowercase font-medium">Gloovoo</p>
